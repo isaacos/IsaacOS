f34930f102c691e8b54b3dcbf247e8e8a0254cb2
isaac <oseasisaac91@gmail.com>
2019-08-19 12:08:55 -0400

    updated text in the resume, fonts, sliding css transition for frontend and backend a

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const description = document.getElementById('description')
 const projectsArray = [
   {
     title: 'Captionr',
-    description: 'A caption contest application where users can post pictures, leave captions, up or downvote captions.',
+    description: 'A caption contest application where users can post pictures, leave captions, and up or downvote captions.',
     liPoints:[
-        'As a group of two, formatted a single page Vanilla JS application to allow users to view all pictures and top voted captions.',
+        'As a group of two, formatted a single page Vanilla JS application to display all pictures and top voted captions.',
         'Developed join table between users and captions to allow users to vote on captions.',
-        'Implemented logic in Javascript that sorts captions by voting score and marks the most upvoted caption.',
+        'Implemented logic in JavaScript that sorts captions by voting score and marks the most upvoted caption.',
         'Styled single page application with custom CSS for a unique and easy-to-navigate look.'
     ],
     video: '<iframe id="iframe" width="560" height="315" src="https://www.youtube.com/embed/cwxn_hTB6M8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
@@ -20,9 +20,9 @@ const projectsArray = [
     title: 'Bgft',
      description: 'A comic application that allows users to report sightings of Bigfoot with locations and photos.',
      liPoints:[
-       'As a group of two, implemented Leaflet to provide users with an interactive map to post and view sightings.',
+       'As a group of two, implemented React-Leaflet to provide users with an interactive map to post and view sightings.',
        'Built model for ease in viewing information and filling out forms.',
-       'Created full CRUD actions to allow users to make edit or delete comments.',
+       'Created full CRUD actions to allow users to make, edit, or delete comments.',
        'Instituted routes that redirect user to login page if they are not signed in.'
       ],
       video: '<iframe  id="iframe" width="560" height="315" src="https://www.youtube.com/embed/M8bjI8HwzdM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
@@ -33,10 +33,10 @@ const projectsArray = [
       title: 'Auth Eats',
       description: 'Yelp style restaurant review website that allows more knowledgable users to leave an authoritative review.',
       liPoints:[
-        'Built search functionality in React components to allow user to search restaurants through the navbar.',
-        'Generated an API based on requests from Yelp’s API to allow frontend developers to easily access the relevant information.',
-        'Linked users with a self referential join table to allow users to grant other users permission to leave authentic reviews.',
-        'Worked with a combination of Materialize and custom CSS for an intuitive and styled user experience.'
+        'Modified code in order to optimize queries to the database using Active Record.',
+        'Constructed multicomponent React/Redux application.',
+        'Populated a custom database with restaurant information using data from Yelp’s API.',
+        'Incorporated privileges within a schema that allows users to modify permissions of other users.'
       ],
       video: '<iframe id="iframe" width="560" height="315" src="https://www.youtube.com/embed/-hGsP7ygPV4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
       gitfront: 'https://github.com/isaacos/auth-eats-frontend',
@@ -58,12 +58,13 @@ titleDiv.innerHTML += `
   <h3 class="title other" id="sub-title">
     <a href="https://www.linkedin.com/in/isaac-oseas-b8b0b0a4/">LinkedIn</a>
     <a href="https://github.com/isaacos">Github</a>
+    <a href="https://medium.com/@oseasisaac91">Blog</a>
     <a href="mailto:oseasisaac91@gmail.com">Email</a>
   </h3>`
 header.insertBefore(titleDiv, header.firstChild)
 
 description.innerHTML += `
-    <p>My first experience with tech was in graduate school when I learned about Linux in order to install it on a cheap laptop. This sparked my interest to learn more about technology as a means to help people. Since then I graduated from the Flatiron School becoming a fullstack developer with experience working with JavaScript, Ruby on Rails, React/Redux, HTML5, CSS. My coding philosophy is that functionality and style should be interconnected, and that the best code is written by people who are always learning.</p>`
+    <p>My first experience with tech was in graduate school when I learned about Linux in order to install it on a cheap laptop. I immediately fell in love with programming because of its potential to connect people and make their lives easier.  Since then I graduated from the Flatiron School  becoming a fullstack developer with experience working with JavaScript, Ruby on Rails, React/Redux, HTML5, CSS. My coding philosophy is that functionality and style should be interconnected, and that the best code is written by people who are always learning.</p>`
 
 
 const projects = document.createElement('DIV')
